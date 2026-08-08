1f472dc529da1a304d719a62c2e1bc3531f19ff4
kiranvibhute <kiran.vibhute3@gmail.com>
2026-08-08 08:40:31 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (51.49%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+
+        int cand1 = 0, cand2 = 0;
+        int count1 = 0, count2 = 0;
+
+        // Find two possible candidates
+        for (int num : nums) {
+            if (num == cand1) {
+                count1++;
+            }
+            else if (num == cand2) {
+                count2++;
+            }
+            else if (count1 == 0) {
+                cand1 = num;
+                count1 = 1;
+            }
+            else if (count2 == 0) {
+                cand2 = num;
+                count2 = 1;
+            }
+            else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Verify candidates
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == cand1) count1++;
+            else if (num == cand2) count2++;
+        }
+
+        vector<int> ans;
+
+        if (count1 > n / 3)
+            ans.push_back(cand1);
+
+        if (count2 > n / 3)
+            ans.push_back(cand2);
+
+        return ans;
+    }
+};
